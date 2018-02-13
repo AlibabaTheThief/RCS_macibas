b31cfe7597b3fecf9671d8b2037f8dcbddf43eea
Alibaba <alibabaiskenderov@gmail.com>
2018-02-13 21:36:12 +0200

    Pievienoju failus

---
+++ Hello_World_app/Hello_World_app/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_World_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NameAndAge();
+            //SayHello();
+            //SayHelloToUser();
+        }
+
+        static void NameAndAge()
+        {
+            Console.WriteLine("Your Name:");
+            string userName;
+            userName = Console.ReadLine();
+            Console.WriteLine("Your Age:");
+            string age;
+            age = Console.ReadLine();
+            Console.WriteLine("Sveiks, " + userName + " ! " + "Tu esi " + age + " gadus vecs.");
+            Console.ReadLine();
+        }
+
+        static void SayHelloToUser()
+        {
+            Console.WriteLine("Ievadiet lietotajvardu:");
+            string userName;
+            userName = Console.ReadLine();
+            Console.WriteLine(userName);
+            Console.ReadLine();
+        }
+
+        static void SayHello()
+        {
+            Console.WriteLine("Hello, world!"); //rada textu
+            Console.ReadLine(); //raada textu
+        }
+    }
+}
